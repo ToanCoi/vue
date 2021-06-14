62ade9f5a3de4878c5654855a228b6b8020f5870
ToanCoi <nguyentoan110720@gmail.com>
2021-06-15 06:36:51 +0700

    update

--- cukcuk/src/components/common/FieldInputLabel.vue
+++ cukcuk/src/components/common/FieldInputLabel.vue
@@ -1,26 +1,27 @@
 <template>
-  <div>
+  <div class="tooltip" :style="{'--scale': scaleTooltip, '--tooltip-message': errorMessage + ''}">
     <label :for="customData.inputId" class="text-label"
       >{{ customData.labelText }}
       <span v-if="customData.isRequired">
         (<span style="color: red">*</span>)
       </span>
     </label>
-    <el-tooltip :popover-style="{background: '#ff4747'}" effect="dark" :content="errorMessage" placement="top">
-      <input
+    <input
       :type="customData.inputType"
       class="field-input"
+      :class="{'invalidInput': invalidInput}"
       :placeholder="customData.placeholder"
       :id="customData.inputId"
+      @focus="focus"
       @blur="blur"
       v-model="customData.model"
     />
-    </el-tooltip>
     
   </div>
 </template>
 
 <script>
+import Resource from '../../js/common/Resource';
 
 export default {
   props: {
@@ -31,16 +32,26 @@ export default {
   },
   data() {
       return {
-          errorMessage: null
+          errorMessage: '',
+          scaleTooltip: 0,
+          invalidInput: false
       }
   },
   methods: {
+      /**
+       * Hàm xử lý sự kiện focus
+       * NVTOAN 14/06/2021
+       */
+      focus() {
+        this.scaleTooltip = 0;
+        this.invalidInput = false;
+      },
       /**
        * Hàm xử lý sự kiện khi không focus vào input nữa (check validate)
        * NVTOAN 14/06/2021
        */
       blur() {
-          this.validate();
+          this.validate();console.log("afsd")
       },
 
       /**
@@ -50,11 +61,20 @@ export default {
       validate() {
           let value = this.customData.model;
 
+          if(typeof this.customData.model == 'object') {
+            value = JSON.stringify(this.customData.model);
+          }
+
           if(this.validateRequired(value)) {
-              console.log("fasf");
+              if(!this.validateDatType(value)) {
+                this.errorMessage = "'" + this.customData.labelText + ' không hợp lệ' + "'";
+                this.invalidInput = true;
+                this.scaleTooltip = 1;
+              }
           } else {
-              this.errorMessage = this.customData.labelText + " không được để trống";
-              console.log(this.errorMessage);
+              this.errorMessage = "'" + this.customData.labelText + ' không được để trống' + "'";
+              this.invalidInput = true;
+              this.scaleTooltip = 1;
           }
 
       },
@@ -71,6 +91,48 @@ export default {
 
           return true;
       },
+
+      /**
+       * Hàm validate theo kiểu dữ liệu
+       * NVTOAN 14/06/2021
+       */
+      validateDatType(value) {
+        let res = true;
+
+        switch(this.customData.dataType) {
+          case Resource.DataTypeColumn.Number:
+            if(isNaN(value)) res = false;
+            break;
+          case Resource.DataTypeColumn.Email:
+            res = this.validateEmail(value);
+            break;
+          case Resource.DataTypeColumn.Date:console.log("asfdf")
+            res = this.validateDate(value);
+            break
+        }
+
+        return res;
+      },
+
+      /**
+       * Validate Email
+       * NVTOAN 14/06/2021
+       */
+      validateEmail(value) {
+        var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        return regex.test(value);
+      },
+
+      /**
+       * Hàm validate ngày tháng
+       * NVTOAN 14/06/2021
+       */
+      validateDate(value) {
+        var regex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$|(?:(?:1[6-9]|[2-9]\d)?\d{2})(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\5(?:0?[1-9]|1\d|2[0-8])$|^(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00)))(\/|-|\.)0?2\6(29)$|^(?:(?:1[6-9]|[2-9]\d)?\d{2})(?:(?:(\/|-|\.)(?:0?[1,3-9]|1[0-2])\8(?:29|30))|(?:(\/|-|\.)(?:0?[13578]|1[02])\9(?:31)))$/;
+        
+        return regex.test(value);
+      }
   }
 };
 </script>
@@ -89,19 +151,50 @@ input:focus {
     border: 1px solid #019160;
     outline: none;
 }
-/* .el-tooltip__popper.is-dark {
-    background: blue;
-    color: -sharpfff;
-} */
 
-.el-tooltip__popper [x-placement ^=left] .popper__arrow ::after {
-   border-left-color:#ff4747;
-  }
-.el-tooltip__popper [x-placement ^=left] .popper__arrow {
- border-left-color:#ff4747;
+.invalidInput {
+  border: 1px solid #ff4747;
+  outline: none;
 }
 
-.draw_share_atooltip {
-   background:#ff4747! important;
- }
+.tooltip {
+    position: relative;
+    --scale: 0;
+    --arrow-size: 10px;
+    --tooltip-color: #ff4747;
+    --tooltip-message: '';
+}
+
+.tooltip::before,
+.tooltip::after {
+    position: absolute;
+    top: 32px;
+    left: 50%;
+    transform: translateX(-50%) translateY(var(--translate-y, 0)) scale(var(--scale));
+    transition: 150ms transform;
+    transform-origin: bottom center;
+    font-size: 12px;
+}
+
+.tooltip::before {
+    --translate-y: calc(-100% - var(--arrow-size));
+
+    content: var(--tooltip-message);
+    color: #fff;
+    padding: .5rem;
+    border-radius: .3rem;
+    text-align: center;
+    width: max-content;
+    max-width: 50%;
+    background-color: var(--tooltip-color);
+}
+
+.tooltip::after {
+    --translate-y: calc(-1 * var(--arrow-size) - 1px);
+
+    content: '';
+    border: 10px solid transparent;
+    border-top-color: var(--tooltip-color);
+    transform-origin: top center;
+}
 </style>
